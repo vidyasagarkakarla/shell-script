22556027a226a5fd21ae39b4990d7db1c3552486
vidyasagarkakarla <vidyasagarkakarla16@gmail.com>
2024-01-11 14:40:12 +0530

    shell script changes

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hello Suresh, Good Morinng"
+echo "Suresh: Hi Ramesh, Very Good Moring"
+echo "Ramesh: How are you Suresh?"
+echo "Suresh: I am good Ramesh, How are you"
