3c2791fec63f9d42a2240831ef23b767739f7422
vidyasagarkakarla <vidyasagarkakarla16@gmail.com>
2024-01-11 14:52:49 +0530

    shell script changes

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: Hello $PERSON2, Good Morinng"
+echo "$PERSON2: Hi $PERSON1, Very Good Moring"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good $PERSON1, How are you"
