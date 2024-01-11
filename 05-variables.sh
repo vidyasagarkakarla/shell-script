5ba8eef858ecdc4ef4975a7b1f80bb78ffb5c7cc
vidyasagarkakarla <vidyasagarkakarla16@gmail.com>
2024-01-11 15:01:29 +0530

    shell script changes

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,5 +1,5 @@
 #!/bib/bash
 
 echo "please enter your Username"
-read USERNAME # the value entered above will be automatically atteched to USERNAME variable
+read -s USERNAME # the value entered above will be automatically atteched to USERNAME variable
 echo "Username is: $USERNAME"
