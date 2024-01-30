635e8da9e052675b3e661eb7dcf590bc506f43ee
vidyasagarkakarla <vidyasagarkakarla16@gmail.com>
2024-01-30 11:32:56 +0530

    shell script

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -11,7 +11,7 @@ USAGE(){
     echo "-h, Display Help and exit"
 }
 
-while getopts ":n:w:h" opt ; do
+while getopts ":n:w:h" opt; do
     case $opt in
         n) NAME="$OPTARG";;
         w) WISHES="$OPTARGS";;
